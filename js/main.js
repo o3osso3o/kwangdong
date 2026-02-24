1f6ec646cd407074eefc3f3a60128f5d8eda50fc
o3osso3o <o3osso3o@gmail.com>
2026-02-24 17:19:02 +0900

    0224 5:18

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
+    AOS.init({
+    disable: false, //  AOS 끄지 않기! → 애니메이션 작동하게 두기
+    startEvent: "DOMContentLoaded", //  HTML이 다 불러와지면 바로 AOS 시작!
+    initClassName: "aos-init", //  AOS가 준비됐다는 표시 클래스 (자동 붙음)
+    animatedClassName: "aos-animate", //  애니메이션이 실행될 때 붙는 클래스 이름
+    useClassNames: false, //  HTML에 data-aos 값 그대로 클래스 안 붙이기
+    disableMutationObserver: false, //  새로 생긴 요소도 자동 감시해서 애니메이션 적용
+    debounceDelay: 50, //  창 크기 바꿀 때 0.05초 기다렸다 계산 (너무 자주 안 하게)
+    throttleDelay: 99, //  스크롤할 때 0.099초마다 한 번씩 체크 (성능 좋게!)
+  });
     // 상단 기사 카드 전체 클릭(데모)
     document.querySelectorAll('.article[data-href]').forEach(card => {
         card.setAttribute('tabindex', '0'); // 키보드 포커스 가능
